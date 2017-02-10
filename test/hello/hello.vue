c829900ac6e71e059850dc74a021376d2be59727
guoyu07 <guoyu07@gmail.com>
2017-02-10 20:35:29 +0800

    add ecmascript

---
+++ test/hello/hello.vue
@@ -0,0 +1,20 @@
+<template>
+  <p>{{ greeting }} world</p>
+</template>
+
+<style scoped>
+  p {
+    font-size: 2em;
+    text-align: center;
+  }
+</style>
+
+<script>
+module.exports = {
+  data: function(){
+    return {
+      greeting: 'Hello'
+    }
+  }
+}
+</script>
